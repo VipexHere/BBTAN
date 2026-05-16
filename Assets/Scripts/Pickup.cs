220894cad745ed9054bcefdd4d8a878844f7c653
Bartosz Doktor <bartoszdoktor2002@gmail.com>
2026-05-17 00:27:40 +0200

    Add pickup visuals - colors and symbols for each pickup type

--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -17,6 +17,50 @@ public class Pickup : MonoBehaviour
     // Has this pickup been used this turn?
     private bool usedThisTurn = false;
 
+    // Reference to the SpriteRenderer component
+    private SpriteRenderer spriteRenderer;
+
+    // References to symbol objects
+    public GameObject symbolPlus;
+    public GameObject symbolScatter;
+    public GameObject symbolHStrike;
+    public GameObject symbolVStrike;
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        SetVisuals();
+    }
+
+    void SetVisuals()
+    {
+        // Hide all symbols first
+        symbolPlus.SetActive(false);
+        symbolScatter.SetActive(false);
+        symbolHStrike.SetActive(false);
+        symbolVStrike.SetActive(false);
+
+        switch (pickupType)
+        {
+            case PickupType.Plus:
+                spriteRenderer.color = new Color(1f, 1f, 0f);
+                symbolPlus.SetActive(true);
+                break;
+            case PickupType.Scatter:
+                spriteRenderer.color = new Color(1f, 0f, 1f);
+                symbolScatter.SetActive(true);
+                break;
+            case PickupType.HorizontalStrike:
+                spriteRenderer.color = new Color(0f, 0.87f, 1f);
+                symbolHStrike.SetActive(true);
+                break;
+            case PickupType.VerticalStrike:
+                spriteRenderer.color = new Color(1f, 0.55f, 1f);
+                symbolVStrike.SetActive(true);
+                break;
+        }
+    }
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Ball"))
